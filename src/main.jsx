8b6236de606d82948c2fce6bc93501a72e0d8760
user <sabarinath02n@gmail.com>
2025-07-23 20:37:58 +0530

    third

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ContextApi from './contextApi/ContextApi.jsx'
+import ContextApi from '../contextApi/ContextApi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
